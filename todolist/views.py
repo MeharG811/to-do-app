dd92cebaf05c0683dd3e07ec2ee5bd61d86f26d1
Hassan Javed <meharhassan811@gmail.com>
2024-05-26 03:45:14 +0500

    first commit 2

--- todolist/views.py
+++ todolist/views.py
@@ -36,9 +36,9 @@ def home(request):
                     "status_":status_,
                     "tasks":tasks
                 }
-                return render(request,"01test.html",{'forms': forms})
+                return render(request,"index.html",{'forms': forms})
         
-    return render(request,"01test.html",{'forms': forms})
+    return render(request,"index.html",{'forms': forms})
 
 def edit_task(request,id):
     task = get_object_or_404(Tasks, id=id)
@@ -50,7 +50,7 @@ def edit_task(request,id):
             return redirect('home')  # Redirect to a view displaying tasks
     else:
         form = TaskForm(instance=task)  # Populate form with existing task data
-    return render(request, '01test.html', {'form': form})
+    return render(request, 'index.html', {'form': form})
     
 def delete_task(request,id):
     status_=status_Return()
