3ec0d9a932f8d9b904c37141b07b029d4992feaa
Hassan Javed <meharhassan811@gmail.com>
2024-05-26 14:23:42 +0500

    first commit

--- todolist/views.py
+++ todolist/views.py
@@ -34,7 +34,20 @@ def home(request):
                 tasks = tasks.filter(
                     Q(title__icontains=query) | Q(description__icontains=query)
                 )
-                task_counts = tasks.values('status').annotate(count=Count('status')).order_by('status')
+                # Initialize counts with zero for each status
+                initial_counts = {status: 0 for status in status_.keys()}
+                
+                # Get the counts from the filtered query
+                task_counts_query = tasks.values('status').annotate(count=Count('status')).order_by('status')
+                
+                # Update the initial counts with the actual counts
+                for entry in task_counts_query:
+                    initial_counts[entry['status']] = entry['count']
+                
+                # Prepare task counts in the format required by the template
+                task_counts = [{'status': status_, 'count': count} for status_, count in initial_counts.items()]
+
+                    
                 forms={
                     "key":1,
                     "status_":status_,
